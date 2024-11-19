45e41723966503e53b17c2661d759bc61dba5577
Poccia <gabrielpoccia@gmail.com>
2024-11-19 12:49:14 -0300

    logging system

--- main.py
+++ main.py
@@ -3,48 +3,94 @@ import time
 import pyperclip
 import webbrowser
 from pyautogui import hotkey, press
+import logging
+from datetime import datetime
+
+"""
+Definir configurações básicas do logging
+"""
+logger = logging.getLogger(__name__)
+log_file_name = f"log_{datetime.now().strftime('%d_%m_%Y_%H_%M')}.log"
+logging.basicConfig(
+    filename=log_file_name, 
+    encoding="utf-8", 
+    level=logging.INFO, 
+    format="[%(asctime)s] %(message)s"
+)
+logger.info(f"Execução iniciada!")
 
 def format_name_phone(row):
-    name, phone = row['NOME'], row['TELEFONE']
-    #name = " ".join([item.capitalize() for item in name.split(' ')])
-    name = name.split(' ')[0].capitalize()
-    phone = "+" + phone
-    return name, phone
+    """Formata o nome e número de telefone para utilização.
+
+    Args:
+        row (series): Linha do dataframe de contatos contendo as informações e um contato
+
+    Returns:
+        str: Retorna o nome e número de telefone formatados.
+    """
+    
+    try:
+        name, phone = row["NOME"], row["TELEFONE"]
+        name = name.split(" ")[0].capitalize()
+        phone = "+" + phone
+        logger.info("Nome e telefone extraídos e formatados com sucesso!")
+        return name, phone
+    
+    except (KeyError, TypeError):
+        logger.error("Não foi possível extrair e formatar nome e telefone do contato!")
+        return None, None
 
 def _web(receiver: str) -> None:
-    """Abre o WhatsApp Web baseado no número de envio"""
+    """Abre o WhatsApp Web baseado no número de envio
+
+    Args:
+        receiver (str): Número de telefone do contato
+    """
     webbrowser.open(
         "https://web.whatsapp.com/send?phone="
         + receiver
     )
     
 def close_tab() -> None:
-    """Fecha a aba ou janela aberta para envio da mensagem"""
-
+    """Fecha a aba ou janela aberta para envio da mensagem
+    """
     time.sleep(2)
     hotkey("ctrl", "w")
 
 def copy_image(path: str) -> None:
-    """Copia a imagem para a área de transferência. Funciona apenas em Windows"""
+    """Copia a imagem para a área de transferência. Funciona apenas em Windows.
+
+    Args:
+        path (str): Caminho para a imagem
+    """
 
     from io import BytesIO
     import win32clipboard  # pip install pywin32
     from PIL import Image
 
-    image = Image.open(path)
-    output = BytesIO()
-    image.convert("RGB").save(output, "BMP")
-    data = output.getvalue()[14:]
-    output.close()
-    win32clipboard.OpenClipboard()
-    win32clipboard.EmptyClipboard()
-    win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
-    win32clipboard.CloseClipboard()
+    try:
+        image = Image.open(path)
+        output = BytesIO()
+        image.convert("RGB").save(output, "BMP")
+        data = output.getvalue()[14:]
+        output.close()
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
+        win32clipboard.CloseClipboard()
+    except (FileNotFoundError, IOError) as e:
+        logger.critical("Erro ao abrir a imagem, execução interrompida: {e}")
+        exit()
 
     
-def send_image(path: str, caption: str, receiver: str) -> None:
-    """Envia uma mensagem para um contato ou um grupo"""
+def send_message(caption, receiver, image_path=None) -> None:
+    """Envia uma mensagem para um contato
 
+    Args:
+        image_path (str): Caminho para a imagem a ser enviada
+        caption (str): Mensagem a ser enviada
+        receiver (str): Número do contato a ser enviada a mensagem
+    """
     # Abre uma janela do WhatsApp web com o contato
     _web(receiver=receiver)
     time.sleep(20)
@@ -55,49 +101,78 @@ def send_image(path: str, caption: str, receiver: str) -> None:
     time.sleep(1)
     
     # Cola a imagem escolhida
-    copy_image(path=path)
-    hotkey("ctrl", "v")
-    time.sleep(1)
+    if image_path:
+        copy_image(path=image_path)
+        hotkey("ctrl", "v")
+        time.sleep(1)
     
     # Envia a mensagem e imagem
     press("enter")
     time.sleep(1)
     
-# Função para carregar o último índice salvo
+
 def get_last_index():
-    if os.path.exists(r'./last_index.txt'):
-        with open(r'./last_index.txt', 'r') as f:
+    """Carregar o último índice salvo
+
+    Returns:
+        int: Índice do último contato para quem foi enviada uma mensagem
+    """
+    if os.path.exists(r"./last_index.txt"):
+        with open(r"./last_index.txt", "r") as f:
             return int(f.read().strip())
     return 0
 
-# Função para salvar o índice atual
+# Função para 
 def save_last_index(index):
-    with open(r'./last_index.txt', 'w') as f:
-        f.write(str(index))
+    """Salvar o índice atual
 
-def send_whatsmessage(df_contacts, message: str, image_path: str):
-    """Envia uma imagem com uma mensagem escrita para uma lista de contatos"""
-    count = 0
-    start_index = get_last_index()  # Pega o último índice salvo
+    Args:
+        index (int): Índice do último contato utilizado
+    """
+    with open(r"./last_index.txt", "w") as f:
+        f.write(str(index))
+    logger.info(f"Último índice registrado!")
+
+def send_whatsmessage(df_contacts, message, limit, image_path=None):
+    """_summary_
+
+    Args:
+        df_contacts (DataFrame): DataFrame dos contatos para quem serão enviadas as mensagens
+        message (str): Mensagem a ser enviada
+        image_path (str): Caminho da imagem a ser enviada com a mensagem
+        limit (int): Número máximo de mensagens a ser enviada
+    """    
+    count = 0 # Índice para registro do número de mensagens enviadas
+    start_index = get_last_index()
+    
     for i, row in df_contacts.iterrows():
-        if count == 1:
+        # Checar limite de mensagens
+        if count == limit:
             break
+        
+        # Pular os contatos cuja mensagem já foi enviada
+        i += 1
         if i <= start_index:
             continue
+        
+        logger.info(" ")
+        logger.info(f"Iniciando execução na linha {start_index + i} da lista de transmissão!")
+        
         name, phone = format_name_phone(row)
+        
+        if not name or not phone:
+            save_last_index(i)
+            continue
+        
         formatted_message = message.format(name)
-        send_image(
-        path=image_path, 
+        send_message(
         caption=formatted_message, 
         receiver=phone,
+        image_path=image_path
         )
+        
+        logger.info(f"Mensagem enviada com sucesso para {name}, número {phone}")
+        
         close_tab()
         save_last_index(i)
         count += 1
-        
-'''if __name__ == '__main__':
-    # Código para testar as funções (opcional)
-    df_contacts = pd.read_csv(...)
-    message = "..."
-    image = "..."
-    send_whatsmessage(df_contacts, message, image)'''
--- run.py
+++ run.py
@@ -1,28 +1,21 @@
 import pandas as pd
-from main import format_name_phone, _web, close_tab, copy_image, send_image, get_last_index, save_last_index, send_whatsmessage
+from main import format_name_phone, _web, close_tab, copy_image, send_message, get_last_index, save_last_index, send_whatsmessage
 
 message = ("""🍫✨ Olá, {}! ✨🍫
 
-Temos uma super novidade para você! 😍 A nossa loja, que antes estava no Shopping Marechal Plaza, agora está de cara nova em um lugar ainda mais especial, esperando por você no endereço: Rua Padre Lustosa, 172 - Centro de São Bernardo do Campo! 🏠
+Esta é uma mensagem teste.""")
 
-E para comemorar essa mudança, preparamos uma promoção imperdível: apresentando esta imagem na loja, você garante 15% de desconto no nosso delicioso fondue! 😋🍓🍫
+#image = r".\Panfleto_Fondue.jpeg"
 
-Venha nos visitar e aproveitar essa delícia com quem você ama! Estamos ansiosos para receber você e tornar seu momento ainda mais doce! 💛
+path_contacts_csv = r".\lista_teste.csv"
 
-📍 Novo endereço: Rua Padre Lustosa, 172 - Centro, São Bernardo do Campo
-⏰ Horário de funcionamento: Seg a Sáb, das 9h às 19h!
-
-Te esperamos aqui! 🥳""")
-
-image = r".\Panfleto_Fondue.jpeg"
-
-path_contacts_csv = r".\Lovers_Loja_3131.csv"
+limit = 2000
 
 df_contacts = pd.read_csv(
     path_contacts_csv,
-    sep=";",
-    encoding='latin1',
-    dtype={'TELEFONE': str}
+    sep=",",
+    encoding="utf-8",
+    dtype={"TELEFONE": str}
 )
 
-send_whatsmessage(df_contacts, message, image)
+send_whatsmessage(df_contacts, message, limit)
